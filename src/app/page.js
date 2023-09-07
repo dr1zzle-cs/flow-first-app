6881787461c53ee669a1a572da450cf99635572b
dr1zzle-cs <myselfraj2910@gmail.com>
2023-09-07 05:56:46 +0530

    commit

--- src/app/page.js
+++ src/app/page.js
@@ -7,46 +7,115 @@ import * as fcl from "@onflow/fcl";
 export default function Home() {
 
   const [user, setUser] = useState({loggedIn: null})
-	// sets default value of loggedIn to null
+  const [name, setName] = useState('')
+  const [transactionStatus, setTransactionStatus] = useState(null) // NEW
 
   useEffect(() => fcl.currentUser.subscribe(setUser), [])
-	// uses FCL to subscribe the user that logs in or signs up
+
+  const sendQuery = async () => {
+    const profile = await fcl.query({
+      cadence: `
+        import Profile from 0xProfile
+
+        pub fun main(address: Address): Profile.ReadOnly? {
+          return Profile.read(address)
+        }
+      `,
+      args: (arg, t) => [arg(user.addr, t.Address)]
+    })
+
+    setName(profile?.name ?? 'No Profile')
+  }
+
+  const initAccount = async () => {
+    const transactionId = await fcl.mutate({
+      cadence: `
+        import Profile from 0xProfile
+
+        transaction {
+          prepare(account: AuthAccount) {
+            // Only initialize the account if it hasn't already been initialized
+            if (!Profile.check(account.address)) {
+              // This creates and stores the profile in the user's account
+              account.save(<- Profile.new(), to: Profile.privatePath)
+
+              // This creates the public capability that lets applications read the profile's info
+              account.link<&Profile.Base{Profile.Public}>(Profile.publicPath, target: Profile.privatePath)
+            }
+          }
+        }
+      `,
+      payer: fcl.authz,
+      proposer: fcl.authz,
+      authorizations: [fcl.authz],
+      limit: 50
+    })
+
+    const transaction = await fcl.tx(transactionId).onceSealed()
+
+    console.log(transaction) 
+  }
+
+ // NEW
+  const executeTransaction = async () => {
+    const transactionId = await fcl.mutate({
+      cadence: `
+        import Profile from 0xProfile
+
+        transaction(name: String) {
+          prepare(account: AuthAccount) {
+            account
+              .borrow<&Profile.Base{Profile.Owner}>(from: Profile.privatePath)!
+              .setName(name)
+          }
+        }
+      `,
+      args: (arg, t) => [arg("MLH", t.String)],
+      payer: fcl.authz,
+      proposer: fcl.authz,
+      authorizations: [fcl.authz],
+      limit: 50
+    })
+
+    fcl.tx(transactionId).subscribe(res => setTransactionStatus(res.status))
+  }
 
   const AuthedState = () => {
-    // function updates the user variable as true or false based on whether a user is logged in or not. 
-
     return (
-	// generates a log out button if loggedIn is true
       <div>
         <div>Address: {user?.addr ?? "No Address"}</div>
+        <div>Profile Name: {name ?? "--"}</div>
+        <div>Transaction Status: {transactionStatus ?? "--"}</div> {/* NEW */}
+        <button onClick={sendQuery}>Send Query</button>
+        <button onClick={initAccount}>Init Account</button>
+  <button onClick={executeTransaction}>Execute Transaction</button> {/* NEW */}
+
         <button onClick={fcl.unauthenticate}>Log Out</button>
-          </div>
-        )
-      }
+      </div>
+    )
+  }
 
-      const UnauthenticatedState = () => {
-        return (
-	// displays default app settings when loggedIn is null
-          <div>
-            <button onClick={fcl.logIn}>Log In</button>
-            <button onClick={fcl.signUp}>Sign Up</button>
-          </div>
-        )
-      }
-
-
-      return (
-        <div>
-          <Head>
-            <title>FCL Quickstart with NextJS</title>
-            <meta name="description" content="My first web3 app on Flow!" />
-            <link rel="icon" href="/favicon.png" />
-          </Head>
-          <h1>Flow App</h1>
-          {user.loggedIn
-            ? <AuthedState />
-            : <UnauthenticatedState />
-          }
-        </div>
-      );
+  const UnauthenticatedState = () => {
+    return (
+      <div>
+        <button onClick={fcl.logIn}>Log In</button>
+        <button onClick={fcl.signUp}>Sign Up</button>
+      </div>
+    )
     }
+  return (
+    <div>
+      <Head>
+        <title>FCL Quickstart with NextJS</title>
+        <meta name="description" content="My first web3 app on Flow!" />
+        <link rel="icon" href="/favicon.png" />
+      </Head>
+      <h1>Flow App</h1>
+      {user.loggedIn
+        ? <AuthedState />
+        : <UnauthenticatedState />
+      }
+    </div>
+  )
+}
+
